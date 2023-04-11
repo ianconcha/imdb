498bfcbeef4e00b0d5755335ffb66e2c4a36c17e
ianconcha <ianfranco.1986@gmail.com>
2023-04-11 01:43:09 -0400

    Add about page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import MenuItem from "./MenuItem";
-import { AiOutlineHome } from "react-icons/ai";
-import { GoInfo } from "react-icons/go";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from "next/link";
 import DarkModeSwitch from "./DarkModeSwitch";
 
@@ -9,21 +8,20 @@ export default function Header() {
   return (
     <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
-        <MenuItem title={"Home"} address={"/"} Icon={AiOutlineHome} />
-        <MenuItem title={"About"} address={"/about"} Icon={GoInfo} />
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
       <div className="flex items-center space-x-5">
         <DarkModeSwitch />
-        <Link href={"/"}>
+        <Link href="/">
           <h2 className="text-2xl">
             <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
-              IMDB
+              IMDb
             </span>
-            <span className="text-xl hidden sm:inline">Clone</span>
+            <span className="text-xl mx-4 hidden sm:inline">Clone</span>
           </h2>
         </Link>
       </div>
-      <div className=""></div>
     </div>
   );
 }
